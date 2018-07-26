c2a8595285520d251e7da25ee9394a8a7b3274f1
Michael Cheng <michael.cheng@flatironschool.com>
2018-07-26 10:01:21 -0400

    pre-lecture auth

---
+++ 43-react-jwt-auth/foodini/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+class Navbar extends React.Component {
+  render(){
+    return (
+      <div className="navbar">
+        <Link to="/recipes/new">Make a New Recipe!</Link>
+        <Link to="/recipes"> Choose Recipes!</Link>
+      </div>
+    )
+  }
+}
+
+export default Navbar
